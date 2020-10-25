f36d1089005bf17c7ab38387d405b9b300105769
Bobbili, Santha Kumar <sbobbili3@gatech.edu>
2020-10-25 17:22:37 -0500

    Database is changed from SQLite to Postgres. Query is added retrieve data from local db.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require "json"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -42,6 +43,7 @@ module FhirApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_record.schema_format = :sql
   end
 end
 
